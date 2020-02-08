4caa432db22680c581f2ecc80907b9d2461b66a2
Lioger <nickstepanov@inbox.ru>
2020-02-08 11:26:44 +0300

    Otskok

--- lect.py
+++ lect.py
@@ -8,16 +8,19 @@ def canvas_click_handler(event):
     print('Hello world! x-', event.x, 'y=', event.y)
 
 def tick():
-    global x, y
-    print("move")
-    x+=1
-    y+=1
-    canvas.move(ball_id, +1, +1)
+    global x, y, dx, dy
+    x += dx
+    y += dy
+    if x + R > WIDTH or x  -R <= 0:
+        dx = -dx
+    elif y + R > HEIGHT or y  -R <= 0:
+        dy = -dy    
+    canvas.move(ball_id, dx, dy)
     root.after(50, tick)
 
 def main():
     global root, canvas
-    global ball_id, x, y, z # T0D0: делать объектно-ориентированный рефакторинг
+    global ball_id, x, y, dx, dy, R # T0D0: сделать объектно-ориентированный рефакторинг
     
     root = tk.Tk()
     root.geometry(str(WIDTH) + "x" + str(HEIGHT))
@@ -28,6 +31,7 @@ def main():
     R = randint(20, 50)
     x = randint(R, WIDTH - R)
     y = randint(R, HEIGHT - R)
+    dx, dy = (+2, +3)
     
     ball_id = canvas.create_oval(x-R, y-R, x+R, y+R, fill='green')
 
